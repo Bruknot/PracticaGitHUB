8095b54de7a50bdb42ddb7cb15d0afb838135230
alumno <alumno@MintEntornos>
2018-03-03 21:14:35 +0100

    Añadidas nuevas funciones
    Se han añadido nuevas funciones a la clase Calculadora y se han incluido
    en la clase Programa

--- PracticaGitHUBJavadoc/src/Calculadora/Programa.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Programa.java
@@ -5,7 +5,17 @@ public class Programa {
 	//TODO: Crear un programa principal que 
 	//utilice todas las funciones de la calculadora
 	//Incluida las operaciones toString y la potencia
-	//que habréis añadido.
+	//que habrï¿½is aï¿½adido.
 	//Ejecutadlo.
-
+	
+	static Calculadora calc = new Calculadora(5,6);
+	
+	public static void main(String[] args) {
+		System.out.println(calc.toString());
+		System.out.println("Suma = " + calc.suma());
+		System.out.println("Resta = " + calc.resta());
+		System.out.println("Division = " + calc.division());
+		System.out.println("Potencia = " + calc.potencia());
+	}
+	
 }
